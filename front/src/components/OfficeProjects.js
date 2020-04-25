0eb1e309a3715fb7335a2d04552398aa5ba43dde
Pierre-Alain8 <pierrealainagn@gmail.com>
2020-04-25 13:25:49 +0200

    front: transciption des components Project et OfficeProject en hooks

--- front/src/components/OfficeProjects.js
+++ front/src/components/OfficeProjects.js
@@ -1,146 +1,139 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Project from './Project';
 
 
-class OfficeProjects extends React.Component { 
-    constructor(props) {
-        super(props)
+const OfficeProjects = (props) =>{
+        // les states & setStates:
+        const [projects, setProjects] = useState([])
+        const [message, setMessage] = useState("")
+        const [values, setValues] = useState({title: "", description: "", content: ""})
     
-        this.state = {
-            title:"",
-            description:"",
-            content:"",
-            projects:[],
-            message:""
-           
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.subformProject = this.subformProject.bind(this);
-     
-    } 
-
-    componentDidMount(){
-        let token = localStorage.getItem('token');
-
-        fetch(`http://localhost:5000/user/getAllProjects`,{
-            headers:{
-                "Authorization": "Bearer " + token
-            },
-            method: 'GET',
-        })
-        .then((res) =>{
-            return res.json()
-        })
-        .then((res) =>{
-            this.setState({
-                projects: res.projectId
+        // ComponentDitMount & ComponentDitUpdate:
+        useEffect(() => {
+            let token = localStorage.getItem('token');
+    
+            fetch(`http://localhost:5000/user/getAllProjects`,{
+                headers:{
+                    "Authorization": "Bearer " + token
+                },
+                method: 'GET',
             })
-            console.log("getAllProjects: ",res);
-            console.log("projects: ",res.projectId);
-           
-        })
-        .catch(error => console.log(error))
-    }
-
-    handleChange(event){
-        let value = event.target.value
-        let name = event.target.name; 
-        
-        this.setState({
-            [name]: value 
-           
-        },console.log(name))
-    }; 
-
-    subformProject(event){
-        event.preventDefault();
-        let token = localStorage.getItem('token');
-        const newProject = {
-                title: this.state.title, 
-                description: this.state.description,
-                content: this.state.content
-            };
-        let projects = [...this.state.projects, newProject]
-        
-
-        fetch(`http://localhost:5000/user/addProject`, {
-            headers:{
-                "Authorization": "Bearer " + token,
-                'Content-Type': 'application/json'  
-            },
-            method: 'POST',
-            body: JSON.stringify(this.state),
-        })
-        .then((res) =>{
-            if(res.status === 200){
-                res.json().then( (res) => {
-                    this.setState({
-                        message:"votre " + this.state.title +" a été enregistré avec succès !",
-                        projects
-                    })
-                    console.log(res)
-                })
-            }else{
-                console.log("hello")
-            }
-        })
-    }
-
+            .then((res) =>{
+                return res.json()
+            })
+            .then((res) =>{
+                // this.setState({
+                //     projects: res.projectId
+                // })
+                setProjects(res.projectId)
+                
+                console.log("getAllProjects: ",res);
+                console.log("projects: ",res.projectId);
+               
+            })
+            .catch(error => console.log(error))
+        }, [])
     
-    render(){
+    
+    
+        const handleChangeProject = (event) =>{
+            const {name, value} = event.target
+            setValues({...values, [name]: value}, console.log(name))
+        }
+    
+        const subformProject = (event) =>{
+            event.preventDefault();
+            let token = localStorage.getItem('token');
+    
+            let newProjects = [...projects, values]
+            
+    
+            fetch(`http://localhost:5000/user/addProject`, {
+                headers:{
+                    "Authorization": "Bearer " + token,
+                    'Content-Type': 'application/json'  
+                },
+                method: 'POST',
+                body: JSON.stringify(values),
+            })
+            .then((res) =>{
+                if(res.status === 200){
+                    res.json().then( (res) => {
+                        setMessage("votre projet a été enregistré avec succès !")
+                        setProjects(newProjects)
+                        console.log(res)
+                    })
+                }else{
+                    console.log("hello")
+                }
+            })
+        }
+    
         return(
             <section className="office-projects tab-content"> 
-                <form className="form-projects" onSubmit={this.subformProject}>
-                    <input type="text" name="title" 
-                        onChange={this.handleChange}
-                        value={this.state.title } 
-                        placeholder="title of project"
-                        className="project-input"
-                    />
-
-                    <input type="text" name="description" 
-                        onChange={this.handleChange} 
-                        value={this.state.description } 
-                        placeholder="description of project" 
-                        className="project-input"
-                    />
-                    <input type="text" name="content" 
-                        onChange={this.handleChange} 
-                        value={this.state.content }
-                        placeholder="content of project" 
-                        className="project-input"
-                    />
-
-                    <button type="submit">Enregistrer</button> 
-                    <p>{this.state.message}</p>
-                </form> 
-
-                <div className="list-projects">
-                    {
-                        this.state.projects.map(project =>{
-                            return(
-                                <Project key={project._id} project={project}/>
-                            )
-                        })
-                    }
-                </div>
-
-            </section>
+                    <form className="form-projects" onSubmit={subformProject}>
+                                 <input type="text" name="title" 
+                                    onChange={handleChangeProject}
+                                    value={values.title} 
+                                    placeholder="title of project"
+                                    className="project-input"
+                                />
+            
+                                <input type="text" name="description" 
+                                    onChange={handleChangeProject} 
+                                    value={values.description } 
+                                    placeholder="description of project" 
+                                    className="project-input"
+                                />
+                                <input type="text" name="content" 
+                                    onChange={handleChangeProject} 
+                                    value={values.content}
+                                    placeholder="content of project" 
+                                    className="project-input"
+                                />
+            
+                                <button type="submit">Enregistrer</button> 
+                                <p>{message}</p>
+                            </form> 
+            
+                            <div className="list-projects">
+                                {
+                                    projects.map((project, index)=>{
+                                        return(
+                                            <Project key={index} project={project}/>
+                                        )
+                                    })
+                                }
+                                {/* {console.log(projects)} */}
+                            </div>
+            
+                        </section>
         )
     }
-}
+
 
 export default OfficeProjects
 
 
-// Les hooks : 
-// const OfficeProjects = (props) =>{
-//     const [projects, setProjects] = useState([])
-//     const [message, setMessage] = useState("")
-//     const [values, setValues] = useState({title: "", description: "", content: ""})
 
+// class OfficeProjects extends React.Component { 
+//     constructor(props) {
+//         super(props)
+    
+//         this.state = {
+//             title:"",
+//             description:"",
+//             content:"",
+//             projects:[],
+//             message:""
+           
+//         }
+//         this.handleChange = this.handleChange.bind(this);
+//         this.subformProject = this.subformProject.bind(this);
+     
+//     } 
 
-//     useEffect(() => {
+//     componentDidMount(){
 //         let token = localStorage.getItem('token');
 
 //         fetch(`http://localhost:5000/user/getAllProjects`,{
@@ -153,37 +146,35 @@ export default OfficeProjects
 //             return res.json()
 //         })
 //         .then((res) =>{
-//             // this.setState({
-//             //     projects: res.projectId
-//             // })
-//             setProjects(res.projectId)
-            
+//             this.setState({
+//                 projects: res.projectId
+//             })
 //             console.log("getAllProjects: ",res);
 //             console.log("projects: ",res.projectId);
            
 //         })
 //         .catch(error => console.log(error))
-//     }, [])
-
-
-
-//     const handleChangeProject = (event) =>{
-//         const {name, value} = event.target
-//         setValues({...values, [name]: value})
-
 //     }
 
-//     const subformProject = (event) =>{
+//     handleChange(event){
+//         let value = event.target.value
+//         let name = event.target.name; 
+        
+//         this.setState({
+//             [name]: value 
+           
+//         },console.log(name))
+//     }; 
+
+//     subformProject(event){
 //         event.preventDefault();
 //         let token = localStorage.getItem('token');
-//         // const newProject = {
-//         //         id: "ProjectId", 
-//         //         title, 
-//         //          description,
-//         //         content
-//         //     };
-
-//         let newProjects = [...projects, values]
+//         const newProject = {
+//                 title: this.state.title, 
+//                 description: this.state.description,
+//                 content: this.state.content
+//             };
+//         let projects = [...this.state.projects, newProject]
         
 
 //         fetch(`http://localhost:5000/user/addProject`, {
@@ -192,13 +183,15 @@ export default OfficeProjects
 //                 'Content-Type': 'application/json'  
 //             },
 //             method: 'POST',
-//             body: JSON.stringify(values),
+//             body: JSON.stringify(this.state),
 //         })
 //         .then((res) =>{
 //             if(res.status === 200){
 //                 res.json().then( (res) => {
-//                     setMessage("votre projet a été enregistré avec succès !")
-//                     setProjects(newProjects)
+//                     this.setState({
+//                         message:"votre " + this.state.title +" a été enregistré avec succès !",
+//                         projects
+//                     })
 //                     console.log(res)
 //                 })
 //             }else{
@@ -207,44 +200,46 @@ export default OfficeProjects
 //         })
 //     }
 
-//     return(
-//         <section className="office-projects tab-content"> 
-//                 <form className="form-projects" onSubmit={subformProject}>
-//                              <input type="text" name="titleProject" 
-//                                 onChange={handleChangeProject}
-//                                 value={values.titleProject } 
-//                                 placeholder="title of project"
-//                                 className="project-input"
-//                             />
-        
-//                             <input type="text" name="description" 
-//                                 onChange={handleChangeProject} 
-//                                 value={values.description } 
-//                                 placeholder="description of project" 
-//                                 className="project-input"
-//                             />
-//                             <input type="text" name="content" 
-//                                 onChange={handleChangeProject} 
-//                                 value={values.content }
-//                                 placeholder="content of project" 
-//                                 className="project-input"
-//                             />
-        
-//                             <button type="submit">Enregistrer</button> 
-//                             <p>{message}</p>
-//                         </form> 
-        
-//                         <div className="list-projects">
-//                             {
-//                                 projects.map((project, index)=>{
-//                                     return(
-//                                         <Project key={index} project={project}/>
-//                                     )
-//                                 })
-//                             }
-//                             {console.log(projects)}
-//                         </div>
-        
-//                     </section>
-//     )
+    
+//     render(){
+//         return(
+//             <section className="office-projects tab-content"> 
+//                 <form className="form-projects" onSubmit={this.subformProject}>
+//                     <input type="text" name="title" 
+//                         onChange={this.handleChange}
+//                         value={this.state.title } 
+//                         placeholder="title of project"
+//                         className="project-input"
+//                     />
+
+//                     <input type="text" name="description" 
+//                         onChange={this.handleChange} 
+//                         value={this.state.description } 
+//                         placeholder="description of project" 
+//                         className="project-input"
+//                     />
+//                     <input type="text" name="content" 
+//                         onChange={this.handleChange} 
+//                         value={this.state.content }
+//                         placeholder="content of project" 
+//                         className="project-input"
+//                     />
+
+//                     <button type="submit">Enregistrer</button> 
+//                     <p>{this.state.message}</p>
+//                 </form> 
+
+//                 <div className="list-projects">
+//                     {
+//                         this.state.projects.map(project =>{
+//                             return(
+//                                 <Project key={project._id} project={project}/>
+//                             )
+//                         })
+//                     }
+//                 </div>
+
+//             </section>
+//         )
+//     }
 // }
--- front/src/components/Project.js
+++ front/src/components/Project.js
@@ -1,120 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+const Project = (props) =>{
+    const {project} = props;
+    // Ce sont les useState qui remplace le this.state et le This.setSate
+    const [deleteLoading, setDeleteLoading] = useState(false);
+    const [values, setValues] = useState({title: "", description: "", content: ""}) ;
+   
+    // Les méthodes
 
-
-class Project extends React.Component { 
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            title:"",
-            description:"",
-            content:"",
-            display: "flex",
-            message:""
-        }
-        this.handleChangeProject = this.handleChangeProject.bind(this);
-        // this.showButtonUpdate = this.showButtonUpdate.bind(this);
-    } 
-
-    handleChangeProject(event){
-        let value = event.target.value
-        let name = event.target.name; 
-        
-        this.setState({
-            [name]: value 
-           
-        },console.log(name))
-    }; 
-
-    render(){
-        const {project} = this.props;
-            
-        return(
-           
-            <div id={project.projectId} className="projects-content">
-
-                <h1>{project.titleProject}</h1>
-
-                <input type="text" name="title" 
-                    placeholder="title of project"
-                    value={this.state.title}
-                    className="project-input"
-                    onChange={this.handleChangeProject}
-
-                />
-                <span>{project.description}</span>
-
-                <input type="text" name="description" 
-                    placeholder="description of project"
-                    value={this.state.description}
-                    className="project-input"
-                    onChange={this.handleChangeProject}
-                />
-                <p>{project.content}</p>
-
-                <input type="text" name="content" 
-                    placeholder="content of project"
-                    value={this.state.content}
-                    className="project-input"
-                    onChange={this.handleChangeProject}
-                />
-
-                <div className="button-container">
-                    <button className="update-project" >
-                        <img src="img/button-update.png" alt="button-update"/>
-                        Modifier
-                    </button>
-
-                    <button className={"validate-project" }>
-                        <img src="img/button-validate.png" alt="button-validate"/>
-                        Actualisé
-                    </button>
-
-                    <button className="delete-project">
-                        <img src="img/button-delete.png" alt="button-delete"/> 
-                        Supprimer
-                    </button>
-                </div>
-            </div>
-
-           
-            
-        )
+    const deleteProject = () =>{ // ce sont les méthodes
+        setDeleteLoading(true)
+        console.log(deleteLoading)
+                
     }
-}
+    const handleChangeProject = (event) =>{
+        const {name, value} = event.target // création de l'event 
+        setValues({...values, [name]: value}) // setState de values en créant une nouvelle entrée dans ls state values  cibler les valeurs des inputs possédant l'attribut name
 
-export default Project
+    }
 
-// Les hooks : 
-// const Project = (props) =>{
-//     const {project} = props;
-//     const [deleteLoading, setDeleteLoading] = useState(false);
-//     // Ce sont les useState qui remplace le this.state et le This.setSate
-//     // Mettre des noms de méthode  parlant
-//     const deleteProject = () =>{ // ce sont les méthodes
-//         setDeleteLoading(true)
-        
-//     }
-
-//  const {project} = props;
-// const dispatch = useDispatch(); 
-// useDispatch consiste à activer les actions du reducer (liste de nos actions)
-// const modalProjectBool = useSelector(state => state.modalProjectBool) 
-// useSelector consiste à rappeler un state général définis dans le store
+// const dispatch = useDispatch(); //  useDispatch consiste à activer les actions du reducer (liste de nos actions)
+// const modalProjectBool = useSelector(state => state.modalProjectBool) // useSelector consiste à rappeler un state général définis dans le store
 // console.log("modalProjectBool: ", modalProjectBool) 
 
-//     return(
+    return(
        
-//         <div id={project.projectId} className="projects-content">
-//             {console.log(deleteLoading)}
-//             <h1>{project.titleProject}</h1>
-//             <span>{project.description}</span>
-//             <p>{project.content}</p>
+        <div id={project.projectId} className="projects-content">
+        
 
-//             <button className="update-project">MODIFIER</button>
-//             <button onClick={deleteProject} className="delete-project">DELETE</button>
-//         </div>
-    
-// )
-// }
+        <h1>{project.title}</h1>
+
+        <input type="text" name="title" 
+            placeholder="title of project"
+            value={values.title}
+            className="project-input"
+            onChange={handleChangeProject} 
+
+        />
+        <span>{project.description}</span>
+
+        <input type="text" name="description" 
+            placeholder="description of project"
+            value={values.description}
+            className="project-input"
+            onChange={handleChangeProject} 
+        />
+        <p>{project.content}</p>
+
+        <input type="text" name="content" 
+            placeholder="content of project"
+            value={values.content}
+            className="project-input"
+            onChange={handleChangeProject} 
+        />
+
+        <div className="button-container">
+            <button className="update-project" >
+                <img src="img/button-update.png" alt="button-update"/>
+                Modifier
+            </button>
+
+            <button className={"validate-project" }>
+                <img src="img/button-validate.png" alt="button-validate"/>
+                Actualisé
+            </button>
+
+            <button onClick={deleteProject} className="delete-project">
+                <img src="img/button-delete.png" alt="button-delete"/> 
+                Supprimer
+            </button>
+        </div>
+    </div>
+
+)
+}
+
+
+export default Project
