2d482387157dabbd36477483d32316cfdd4adc5e
Pierre-Alain8 <pierrealainagn@gmail.com>
2020-04-25 15:28:53 +0200

    front: retransciption du component RegisterUser en hooks

--- front/src/components/OfficeUser.js
+++ front/src/components/OfficeUser.js
@@ -1,48 +1,57 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { withRouter} from 'react-router-dom';
 import '../css/officeUser.css'; 
 import OfficeProfile from './OfficeProfile';
 import OfficeProjects from './OfficeProjects';
 import OfficeLinks from './OfficeLinks';
 
+const OfficeUser = (props) =>{
 
-class OfficeUser extends React.Component { 
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            active: "active"
-        }   
-    } 
-
-    componentDidMount(){
-
+   useEffect(() =>{
         let token = localStorage.getItem('token')
         if(!token)
-         this.props.history.push('/');
-        
-    };
-    
-    
-    render(){
-        return(
-            <section className="officeUser"> 
-            
-                <OfficeProfile /> 
-                <div className="tabs">
-                    <button className="tabsLink active">
-                        <p data-title="Projects">PROJETS</p>
-                    </button>
-                    <button className="tabsLink">
-                        <p data-title="Links">LINKS</p>
-                    </button>
-                </div>
-                <OfficeProjects />
-                <OfficeLinks />
-               
-            </section>
-        )
-    }
+        this.props.history.push('/');
+   })
+
+   return(
+    <section className="officeUser">     
+        <OfficeProfile /> 
+        <OfficeProjects />
+        <OfficeLinks />
+    </section>
+   )
 }
 
 export default withRouter(OfficeUser)
+
+
+// class OfficeUser extends React.Component { 
+//     constructor(props) {
+//         super(props)
+    
+//         this.state = {
+//             active: "active"
+//         }   
+//     } 
+
+//     componentDidMount(){
+
+//         let token = localStorage.getItem('token')
+//         if(!token)
+//          this.props.history.push('/');
+        
+//     };
+    
+    
+//     render(){
+//         return(
+//             <section className="officeUser"> 
+            
+//                 <OfficeProfile /> 
+//                 <OfficeProjects />
+//                 <OfficeLinks />
+               
+//             </section>
+//         )
+//     }
+// }
--- front/src/components/RegisterUser.js
+++ front/src/components/RegisterUser.js
@@ -1,76 +1,60 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import { Link } from 'react-router-dom';
 import { withRouter} from 'react-router-dom';
 import '../css/registerUser.css';
-// import dotenv from 'dotenv'; 
 
 
 
-class RegisterUser extends React.Component {
+const RegisterUser =(props) =>{
+    // Les states
+    const [values, setValues] = useState({
+        name: "", 
+        pseudo: "", 
+        email: "", 
+        password:"", 
+        password2:""
+    })
+    const [error, setError] = useState("")
 
-    constructor(props) {
-        super(props); 
-
-        this.state = {
-            name: "", 
-            pseudo: "", 
-            email: "", 
-            password: "",
-            password2:"",
-            error : ""
-            
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        
-    } 
-
-    handleChange(event) { 
+    // Les méthodes:
+    const handleChange = (event) => { 
         // l'évenement permettant de ciblier les valeurs des inputs
-        const target = event.target;
-        const value = target.value;
-        // l'évenement event.target est rappelé dans les variables let pour cibler les input
-        let name =target.name;
-        
-        this.setState({
-            // récupération des valeurs de manières indépendantes 
-            [name]:value,
-
-        }, console.log(name))
-
+        const {name, value} = event.target
+        setValues({...values, [name]: value}, console.log(name))
+        // récupération des valeurs de manières indépendantes 
     }; 
 
-    handleSubmit(event) { 
+    const handleSubmit = (event) =>{ 
         event.preventDefault();
-    
 
         fetch(`http://localhost:5000/user/register` , {
             headers:{
                 'Content-Type': 'application/json'  
             },
             method: 'POST',
-            body: JSON.stringify(this.state),
+            body: JSON.stringify(values),
         })
         .then((res) => {
             switch (res.status) {
                 case 401:
-                    this.setState({
-                        error:"Votre password doit contenir au moins 1 lettre minuscule, 1 lettre majuscule, 1 caractère numérique et de plus de 8 caractères"
-                    })
+                    setError("Votre password doit contenir au moins 1 lettre minuscule, 1 lettre majuscule, 1 caractère numérique et de plus de 8 caractères")
+                    // this.setState({
+                    //     error:"Votre password doit contenir au moins 1 lettre minuscule, 1 lettre majuscule, 1 caractère numérique et de plus de 8 caractères"
+                    // })
                     console.log(res)
                     break;
 
                 case 409: 
-                    this.setState({
-                        error:"Votre password ne correspond pas au précédent, veuillez le confirmer"
-                    })
+                setError("Votre password ne correspond pas au précédent, veuillez le confirmer")
+                    // this.setState({
+                    //     error:"Votre password ne correspond pas au précédent, veuillez le confirmer"
+                    // })
                     console.log(res)
                     break;
                 case 200:
                     console.log(res)
-                    console.log(this.state.password)
-                    this.props.history.push('/LoginUser')
+                    console.log(values.password)
+                    props.history.push('/LoginUser')
                     break;
             
                 default:
@@ -81,13 +65,10 @@ class RegisterUser extends React.Component {
         })
         .catch((errors) => {console.log(errors)})
          
-
-        // console.log( this.state.pseudo, this.state.name, this.state.email, this.state.pseudo, this.state.password)
-        console.log(this.state)
+        console.log(values)
     };
-
-    render(){
-        return(
+    
+    return(
         <section className="section-register">
 
             <article className="presentation-uniarts">
@@ -104,7 +85,7 @@ class RegisterUser extends React.Component {
 
             </article>
 
-            <form className="form-register" onSubmit={this.handleSubmit }> 
+            <form className="form-register" onSubmit={handleSubmit }> 
 
                 <h2>REGISTER</h2>
 
@@ -112,8 +93,8 @@ class RegisterUser extends React.Component {
                     Name : 
                     <input type="text" id="name" name="name"  
                     placeholder="Enter your name"
-                    value={this.state.name } 
-                    onChange={this.handleChange } />
+                    value={values.name } 
+                    onChange={handleChange } />
 
                 </label> 
 
@@ -121,8 +102,8 @@ class RegisterUser extends React.Component {
                     Pseudo : 
                     <input type="text" id="pseudo" name="pseudo"  
                     placeholder="Enter your pseudo"
-                    value={this.state.pseudo } 
-                    onChange={this.handleChange } />
+                    value={values.pseudo } 
+                    onChange={handleChange } />
 
                 </label>
 
@@ -130,8 +111,8 @@ class RegisterUser extends React.Component {
                     Email : 
                     <input type="email" id="email" name="email" 
                     placeholder="Enter your email"
-                    value={this.state.email} 
-                    onChange={this.handleChange} />
+                    value={values.email} 
+                    onChange={handleChange} />
 
                 </label>
 
@@ -139,8 +120,8 @@ class RegisterUser extends React.Component {
                     Password : 
                     <input type="password" id="password" name="password"  
                     placeholder="Enter your password"
-                    value={this.state.password} 
-                    onChange={this.handleChange} />
+                    value={values.password} 
+                    onChange={handleChange} />
                     
                 </label>
 
@@ -148,8 +129,8 @@ class RegisterUser extends React.Component {
                     Password confirm : 
                     <input type="password" id="password" name="password2"  
                     placeholder="Enter your password"
-                    value={this.state.password2} 
-                    onChange={this.handleChange} />
+                    value={values.password2} 
+                    onChange={handleChange} />
                     
                 </label>
 
@@ -161,7 +142,7 @@ class RegisterUser extends React.Component {
 
                 <div className="errorRegister">
                     <p>
-                        {this.state.error}
+                        {error}
                     </p>
                 </div>
               
@@ -169,7 +150,174 @@ class RegisterUser extends React.Component {
         </section>
          
         )
-    }
+
 }
 
+
 export default withRouter(RegisterUser)
+
+// class RegisterUser extends React.Component {
+
+//     constructor(props) {
+//         super(props); 
+
+//         this.state = {
+//             name: "", 
+//             pseudo: "", 
+//             email: "", 
+//             password: "",
+//             password2:"",
+//             error : ""
+            
+//         }
+
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+        
+//     } 
+
+//     handleChange(event) { 
+//         // l'évenement permettant de ciblier les valeurs des inputs
+//         const target = event.target;
+//         const value = target.value;
+//         // l'évenement event.target est rappelé dans les variables let pour cibler les input
+//         let name =target.name;
+        
+//         this.setState({
+//             // récupération des valeurs de manières indépendantes 
+//             [name]:value,
+
+//         }, console.log(name))
+
+//     }; 
+
+//     handleSubmit(event) { 
+//         event.preventDefault();
+    
+
+//         fetch(`http://localhost:5000/user/register` , {
+//             headers:{
+//                 'Content-Type': 'application/json'  
+//             },
+//             method: 'POST',
+//             body: JSON.stringify(this.state),
+//         })
+//         .then((res) => {
+//             switch (res.status) {
+//                 case 401:
+//                     this.setState({
+//                         error:"Votre password doit contenir au moins 1 lettre minuscule, 1 lettre majuscule, 1 caractère numérique et de plus de 8 caractères"
+//                     })
+//                     console.log(res)
+//                     break;
+
+//                 case 409: 
+//                     this.setState({
+//                         error:"Votre password ne correspond pas au précédent, veuillez le confirmer"
+//                     })
+//                     console.log(res)
+//                     break;
+//                 case 200:
+//                     console.log(res)
+//                     console.log(this.state.password)
+//                     this.props.history.push('/LoginUser')
+//                     break;
+            
+//                 default:
+//                     break;
+//             }
+//             return res.json()
+            
+//         })
+//         .catch((errors) => {console.log(errors)})
+         
+
+//         // console.log( this.state.pseudo, this.state.name, this.state.email, this.state.pseudo, this.state.password)
+//         console.log(this.state)
+//     };
+
+//     render(){
+//         return(
+//         <section className="section-register">
+
+//             <article className="presentation-uniarts">
+
+//                 <h1>Un collectif d'artises francophones...</h1>
+
+//                 <p>
+//                     UniArts est un collectif ayant pour but de mettre en avant divers artistes francophones meritent plus de visibilités, UniArtsts présentera chacun des artistes qui souhaitent rejoindre le collectif. 
+//                 </p> 
+
+//                 <p> 
+//                     Car oui...Libre à vous de le rejoindre comme bon vous semble. Il vous sera aussi possible de présenter vos projets, indiquer les liens où on peut découvrir vos travaux, suivre votre activité !  
+//                 </p>
+
+//             </article>
+
+//             <form className="form-register" onSubmit={this.handleSubmit }> 
+
+//                 <h2>REGISTER</h2>
+
+//                 <label> 
+//                     Name : 
+//                     <input type="text" id="name" name="name"  
+//                     placeholder="Enter your name"
+//                     value={this.state.name } 
+//                     onChange={this.handleChange } />
+
+//                 </label> 
+
+//                 <label> 
+//                     Pseudo : 
+//                     <input type="text" id="pseudo" name="pseudo"  
+//                     placeholder="Enter your pseudo"
+//                     value={this.state.pseudo } 
+//                     onChange={this.handleChange } />
+
+//                 </label>
+
+//                 <label> 
+//                     Email : 
+//                     <input type="email" id="email" name="email" 
+//                     placeholder="Enter your email"
+//                     value={this.state.email} 
+//                     onChange={this.handleChange} />
+
+//                 </label>
+
+//                 <label> 
+//                     Password : 
+//                     <input type="password" id="password" name="password"  
+//                     placeholder="Enter your password"
+//                     value={this.state.password} 
+//                     onChange={this.handleChange} />
+                    
+//                 </label>
+
+//                 <label> 
+//                     Password confirm : 
+//                     <input type="password" id="password" name="password2"  
+//                     placeholder="Enter your password"
+//                     value={this.state.password2} 
+//                     onChange={this.handleChange} />
+                    
+//                 </label>
+
+//                 <button type="submit">INSCRIPTION</button>
+
+//                 <div>
+//                     <Link to="/loginUser"><p>Vous avez déjà un compte ?</p></Link>
+//                 </div>
+
+//                 <div className="errorRegister">
+//                     <p>
+//                         {this.state.error}
+//                     </p>
+//                 </div>
+              
+//             </form>
+//         </section>
+         
+//         )
+//     }
+// }
